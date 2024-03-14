365b3f52fee9ce9623b149affc35cc7a611643ef
mur188-235 <159010684+mur188-235@users.noreply.github.com>
2024-03-14 23:38:57 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,12 @@
+document.write("Muhammad Murtuza")
+
+var myStirng="Superman"
+for(i=myStirng.length-1;i>=0;i--){
+    console.log(i,myStirng[i]);
+}
+forDataType=prompt("Enter Number")
+if(!isNaN(forDataType)){
+    alert("Type : number")
+} else{
+    alert("Type " + typeof forDataType)
+}
